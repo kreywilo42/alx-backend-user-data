8a9d576a429779a060353c1a0b4c7bbb6efd40b9
kkmanuu <kreywilo42@gmail.com>
2023-02-15 12:11:15 +0300

    user_authentication

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,6 +3,7 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 from user import Base, User
@@ -39,7 +40,7 @@ class DB:
 
     def find_user_by(self, **kwargs: Dict) -> User:
         """returns user based on the argument"""
-        return self._session.query(User).filter_by(**kwargs).one()
+        self._session.query(User).filter_by(**kwargs).one()
 
     def update_user(self, user_id: int, **kwargs: Dict) -> None:
         """updates a user based on keyword arguments"""
